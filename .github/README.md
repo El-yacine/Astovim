7ffc40ba60284c7b1c5bde2b2c128e47605ce386
Yacine El attaoui <65869939+El-yacine@users.noreply.github.com>
2023-04-06 21:16:15 +0000

    Initial commit

---
+++ .github/README.md
@@ -0,0 +1,40 @@
+# Let's make vim awesome again!
+
+<img src="./screenshot.png" alt="screenshot"/>
+
+- first remove the og gg vim `uninstall vim`
+- Then install nvim in your machine `https://github.com/neovim/neovim/wiki/Installing-Neovim`
+
+## 🛠️ Plugins Installation
+
+#### Make a backup of your current nvim and shared folder
+
+```shell
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+```
+
+#### Clone AstroNvim
+
+```shell
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+```
+
+#### Create a new user repository from this template
+
+Press the "Use this template" button above to create a new repository to store your user configuration.
+
+You can also just clone this repository directly if you do not want to track your user configuration in GitHub.
+
+#### Clone the repository
+
+```shell
+git clone https://github.com/<your_user>/<your_repository> ~/.config/nvim/lua/user
+```
+
+#### Start Neovim
+
+```shell
+nvim
+```
+### Thats all you need to do, now you are ready to start your awesome coding projects 🫰 
---
+++ plugins/community.lua
@@ -0,0 +1,29 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.colorscheme.gruvbox" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.onigiri" },
+  { import = "astrocommunity.colorscheme.oxocarbon" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.media.drop-nvim" },
+}
